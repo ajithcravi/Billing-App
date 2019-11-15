ffee19adc7c2335ba38ce3c391c604099d248619
ajithcravi <craviajith@gmail.com>
2019-11-15 11:50:23 +0530

    nodemon installed

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
 
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.get("/", (req, res, next) => {
   console.log("I am a node server");
   return next();
 });
 
-app.listen(port, (req, res) => {
+app.listen(port, () => {
   console.log(`I am listening in port ${port}`);
 });
